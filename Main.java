7a0e52aada99d6bc16b53f8edb022a03dc87cfa9
anasali321 <154501187+anasali321@users.noreply.github.com>
2025-11-16 23:45:14 -0300

    Initial Java code

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello from Java in VS Code!");
+
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("The add of " + a + " and " + b + " is: " + sum);
+
+    }
+}
